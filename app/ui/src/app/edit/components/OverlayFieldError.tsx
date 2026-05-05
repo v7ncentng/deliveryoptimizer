a6da6c442caf570f03f7a82f72e11dfb5c1797ee
Guritfak Gill <guritfak2007@gmail.com>
2026-05-04 22:43:31 -0700

    chore(edit): update aria attributes for accessibility and improve button titles

--- app/ui/src/app/edit/components/OverlayFieldError.tsx
+++ app/ui/src/app/edit/components/OverlayFieldError.tsx
@@ -9,7 +9,7 @@ const WARNING_ICON = (
     height="24"
     viewBox="0 0 24 24"
     fill="none"
-    aria-hidden
+    aria-hidden="true"
   >
     <path
       d="M10.29 3.86 1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"
